f0b95953ab6ccb454b45fdd8f76fecc201cc47da
Raymond <raymondtheboss1@gmail.com>
2025-05-19 17:09:35 -0400

    Update upbit.js

--- src/exchanges/upbit.js
+++ src/exchanges/upbit.js
@@ -1,5 +1,6 @@
 const Exchange = require('../exchange')
 const Big = require('big.js')
+const { sleep } = require('../helper')
 
 class Upbit extends Exchange {
   constructor() {
@@ -26,11 +27,21 @@ class Upbit extends Exchange {
       return false;
     }
 
-    const added = !this.locallySubscribedPairs.has(pair);
-    if (added) {
+    if (!this.locallySubscribedPairs.has(pair)) {
       this.locallySubscribedPairs.add(pair);
-      console.log(`[${this.id}] Queued subscription for ${pair}. Total locally subscribed pairs: ${this.locallySubscribedPairs.size}`);
     }
+
+    const subscriptionMessage = [
+      { ticket: `aggr-UPBIT-${Date.now()}` },
+      { type: 'trade', codes: [pair] },
+      { format: 'DEFAULT' }
+    ];
+    const messageStr = JSON.stringify(subscriptionMessage);
+
+    console.log(`[${this.id}] Sending subscription for ${pair} on API ${api.id}. Message: ${messageStr}`);
+    api.send(messageStr);
+
+    await sleep(250);
 
     return true;
   }
@@ -42,7 +53,7 @@ class Upbit extends Exchange {
 
     const removed = this.locallySubscribedPairs.delete(pair);
     if (!removed) {
-      // console.log(`[${this.id}] Pair ${pair} was not in locallySubscribedPairs for unsubscription.`);
+      console.log(`[${this.id}] Pair ${pair} was not in locallySubscribedPairs for unsubscription.`);
     } else {
       console.log(`[${this.id}] Unsubscribed ${pair}. Total locally subscribed pairs: ${this.locallySubscribedPairs.size}`);
     }
@@ -91,14 +102,6 @@ class Upbit extends Exchange {
       tradeData.stream_type !== 'REALTIME' // Upbit sends 'SNAPSHOT' then 'REALTIME'
     ) {
       console.debug(`[${this.id}] Received non-trade or non-realtime message on API ${api.id}: Type: ${tradeData.type}, Stream: ${tradeData.stream_type}, Code: ${tradeData.code}. Full data:`, tradeData);
-      // Allow SNAPSHOT messages if they contain trade-like data for initial population,
-      // but ensure they are for a subscribed pair.
-      if (tradeData.type === 'trade' && tradeData.code && this.locallySubscribedPairs.has(tradeData.code)) {
-         // Potentially process snapshot trades if structure is compatible
-         // console.log(`[${this.id}] Processing trade snapshot for ${tradeData.code} on API ${api.id}`);
-      } else {
-        return;
-      }
     }
     
     if (!this.locallySubscribedPairs.has(tradeData.code)) {
@@ -107,30 +110,6 @@ class Upbit extends Exchange {
     }
 
     return this.emitTrades(api.id, [this.formatTrade(tradeData)]);
-  }
-
-  onApiCreated(api) {
-    console.log(`[${this.id}] API ${api.id} created. WebSocket URL: ${api.ws ? api.ws.url : 'N/A'}`);
-    this.startKeepAlive(api, 'PING', 60000); // Standard keep-alive
-
-    const pairsToSubscribe = Array.from(this.locallySubscribedPairs);
-    if (pairsToSubscribe.length > 0) {
-      const subscriptionMessage = [
-        { ticket: `aggr-${api.id}-${Date.now()}` },
-        { type: 'trade', codes: pairsToSubscribe },
-        { format: 'DEFAULT' }
-      ];
-      const messageStr = JSON.stringify(subscriptionMessage);
-      console.log(`[${this.id}] Sending subscription message to API ${api.id} for pairs (${pairsToSubscribe.length}): ${pairsToSubscribe.join(',')}. Message: ${messageStr}`);
-      api.send(messageStr);
-    } else {
-      console.log(`[${this.id}] API ${api.id} created, but no pairs are currently queued for subscription in locallySubscribedPairs.`);
-    }
-  }
-
-  onApiRemoved(api) {
-    console.log(`[${this.id}] API ${api.id} removed. WebSocket URL: ${api.ws ? api.ws.url : 'N/A'}`);
-    this.stopKeepAlive(api);
   }
 }
 
