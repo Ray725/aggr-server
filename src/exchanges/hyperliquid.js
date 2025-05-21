531b5e73c2a7bdec493f5f755111909a4cfaaa1a
Raymond <raymondtheboss1@gmail.com>
2025-05-21 16:54:36 -0400

    Format, add Hyperliquid

---
+++ src/exchanges/hyperliquid.js
@@ -0,0 +1,100 @@
+const Exchange = require('../exchange')
+
+class HYPERLIQUID extends Exchange {
+  constructor() {
+    super()
+    this.id = 'HYPERLIQUID'
+    this.subscriptions = {}
+    this.endpoints = {
+      PRODUCTS: 'https://vfa-microservice.fly.dev/get-hl-pairs'
+    }
+    this.maxConnectionsPerApi = 100
+    this.delayBetweenMessages = 250
+  }
+
+  async getUrl() {
+    return `wss://api.hyperliquid.xyz/ws`
+  }
+
+  formatProducts(data) {
+    return data
+  }
+
+  /**
+   * Sub
+   * @param {WebSocket} api
+   * @param {string} pair
+   */
+  async subscribe(api, pair) {
+    if (!(await super.subscribe.apply(this, arguments))) {
+      return
+    }
+
+    const coin = pair.split('-')[0] // Assuming format COIN-USD
+    const subscription = { type: 'trades', coin }
+    const message = {
+      method: 'subscribe',
+      subscription
+    }
+
+    api.send(JSON.stringify(message))
+    this.subscriptions[pair] = subscription // Store the subscription part for unsubscribing
+
+    return true
+  }
+
+  /**
+   * Sub
+   * @param {WebSocket} api
+   * @param {string} pair
+   */
+  async unsubscribe(api, pair) {
+    if (!(await super.unsubscribe.apply(this, arguments))) {
+      return false
+    }
+
+    const originalSubscription = this.subscriptions[pair]
+    if (!originalSubscription) {
+      console.warn(`No subscription found for ${pair} to unsubscribe.`)
+      return false
+    }
+
+    const message = {
+      method: 'unsubscribe',
+      subscription: originalSubscription
+    }
+
+    api.send(JSON.stringify(message))
+
+    return true
+  }
+
+  formatTrade(trade, pairCoin) {
+    return {
+      exchange: this.id,
+      pair: `${pairCoin.toUpperCase()}-USD`,
+      timestamp: trade.time,
+      price: +trade.px,
+      size: +trade.sz,
+      side: trade.side === 'B' ? 'buy' : 'sell'
+    }
+  }
+
+  onMessage(event, api) {
+    const json = JSON.parse(event.data)
+
+    // Based on the example, trades are in json.data array
+    if (json.channel === 'trades' && Array.isArray(json.data)) {
+      const tradesToEmit = []
+      for (const tradeData of json.data) {
+        // The 'coin' field from the trade data is used to form the pair
+        tradesToEmit.push(this.formatTrade(tradeData, tradeData.coin))
+      }
+      if (tradesToEmit.length > 0) {
+        return this.emitTrades(api.id, tradesToEmit)
+      }
+    }
+  }
+}
+
+module.exports = HYPERLIQUID
--- src/exchanges/upbit.js
+++ src/exchanges/upbit.js
@@ -23,27 +23,31 @@ class Upbit extends Exchange {
 
   async subscribe(api, pair) {
     if (!(await super.subscribe.apply(this, arguments))) {
-      console.warn(`[${this.id}] super.subscribe failed for pair ${pair} on api ${api.id}`);
-      return false;
+      console.warn(
+        `[${this.id}] super.subscribe failed for pair ${pair} on api ${api.id}`
+      )
+      return false
     }
 
     if (!this.locallySubscribedPairs.has(pair)) {
-      this.locallySubscribedPairs.add(pair);
+      this.locallySubscribedPairs.add(pair)
     }
 
     const subscriptionMessage = [
       { ticket: `aggr-UPBIT-${Date.now()}` },
       { type: 'trade', codes: [pair] },
       { format: 'DEFAULT' }
-    ];
-    const messageStr = JSON.stringify(subscriptionMessage);
+    ]
+    const messageStr = JSON.stringify(subscriptionMessage)
 
-    console.log(`[${this.id}] Sending subscription for ${pair} on API ${api.id}. Message: ${messageStr}`);
-    api.send(messageStr);
+    console.log(
+      `[${this.id}] Sending subscription for ${pair} on API ${api.id}. Message: ${messageStr}`
+    )
+    api.send(messageStr)
 
-    await sleep(250);
+    await sleep(250)
 
-    return true;
+    return true
   }
 
   async unsubscribe(_, pair) {
@@ -51,11 +55,15 @@ class Upbit extends Exchange {
       return
     }
 
-    const removed = this.locallySubscribedPairs.delete(pair);
+    const removed = this.locallySubscribedPairs.delete(pair)
     if (!removed) {
-      console.log(`[${this.id}] Pair ${pair} was not in locallySubscribedPairs for unsubscription.`);
+      console.log(
+        `[${this.id}] Pair ${pair} was not in locallySubscribedPairs for unsubscription.`
+      )
     } else {
-      console.log(`[${this.id}] Unsubscribed ${pair}. Total locally subscribed pairs: ${this.locallySubscribedPairs.size}`);
+      console.log(
+        `[${this.id}] Unsubscribed ${pair}. Total locally subscribed pairs: ${this.locallySubscribedPairs.size}`
+      )
     }
   }
 
@@ -73,42 +81,61 @@ class Upbit extends Exchange {
   }
 
   onMessage(event, api) {
-    let tradeData;
+    let tradeData
     try {
       const messageString =
         event.data instanceof ArrayBuffer
           ? new TextDecoder().decode(event.data)
-          : event.data;
-      console.debug(`[${this.id}] Received message on API ${api.id}:`, messageString);
-      tradeData = JSON.parse(messageString);
+          : event.data
+      console.debug(
+        `[${this.id}] Received message on API ${api.id}:`,
+        messageString
+      )
+      tradeData = JSON.parse(messageString)
     } catch (e) {
-      console.warn(`[${this.id}] Failed to parse JSON message on API ${api.id}:`, event.data, e);
-      return;
+      console.warn(
+        `[${this.id}] Failed to parse JSON message on API ${api.id}:`,
+        event.data,
+        e
+      )
+      return
     }
 
     if (!tradeData) {
-      console.debug(`[${this.id}] Parsed message is null or undefined on API ${api.id}. Original:`, event.data);
-      return;
+      console.debug(
+        `[${this.id}] Parsed message is null or undefined on API ${api.id}. Original:`,
+        event.data
+      )
+      return
     }
 
     if (tradeData.type === 'status' || tradeData.status) {
-      console.log(`[${this.id}] Received status/info message on API ${api.id}:`, tradeData);
-      return;
+      console.log(
+        `[${this.id}] Received status/info message on API ${api.id}:`,
+        tradeData
+      )
+      return
     }
 
     if (
       tradeData.type !== 'trade' ||
       tradeData.stream_type !== 'REALTIME' // Upbit sends 'SNAPSHOT' then 'REALTIME'
     ) {
-      console.debug(`[${this.id}] Received non-trade or non-realtime message on API ${api.id}: Type: ${tradeData.type}, Stream: ${tradeData.stream_type}, Code: ${tradeData.code}. Full data:`, tradeData);
-    }
-    
-    if (!this.locallySubscribedPairs.has(tradeData.code)) {
-      console.warn(`[${this.id}] Received message for non-subscribed pair ${tradeData.code} on API ${api.id}:`, tradeData);
-      return;
+      console.debug(
+        `[${this.id}] Received non-trade or non-realtime message on API ${api.id}: Type: ${tradeData.type}, Stream: ${tradeData.stream_type}, Code: ${tradeData.code}. Full data:`,
+        tradeData
+      )
     }
 
-    return this.emitTrades(api.id, [this.formatTrade(tradeData)]);
+    if (!this.locallySubscribedPairs.has(tradeData.code)) {
+      console.warn(
+        `[${this.id}] Received message for non-subscribed pair ${tradeData.code} on API ${api.id}:`,
+        tradeData
+      )
+      return
+    }
+
+    return this.emitTrades(api.id, [this.formatTrade(tradeData)])
   }
 }
 
